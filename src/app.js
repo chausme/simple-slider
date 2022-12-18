34f6cf403209f5d087d04e85f59eb24569fc46b9
Alex Chaus <chausme@gmail.com>
2022-12-18 15:24:07 +0400

    Add logic

--- src/app.js
+++ src/app.js
@@ -1,1 +1,41 @@
+class Slider {
+    #slides = document.querySelectorAll('.slide');
 
+    #btnNext = document.querySelector('.btn-next');
+
+    #btnPrev = document.querySelector('.btn-prev');
+
+    #counter = 0;
+
+    init = () => {
+        this.#slides.forEach((slide, index) => {
+            slide.style.left = `${index * 100}%`;
+        });
+
+        this.#btnNext.addEventListener('click', () => {
+            if (this.#counter === this.#slides.length - 1) {
+                this.#counter = 0;
+            } else {
+                this.#counter += 1;
+            }
+            this.#updateSlides();
+        });
+
+        this.#btnPrev.addEventListener('click', () => {
+            if (this.#counter === 0) {
+                this.#counter = this.#slides.length - 1;
+            } else {
+                this.#counter -= 1;
+            }
+            this.#updateSlides();
+        });
+    };
+
+    #updateSlides = () => {
+        this.#slides.forEach(slide => {
+            slide.style.transform = `translateX(-${this.#counter * 100}%`;
+        });
+    };
+}
+
+new Slider().init();
